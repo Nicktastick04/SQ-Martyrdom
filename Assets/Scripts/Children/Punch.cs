f3525fa43fe82c40c1a74167b724491bf2dbcbe9
Nicholas <nichovasquez04@gmail.com>
2021-12-06 22:57:09 -0800

    Added base script and punch
    The basis for the state machine has been created just needs to be further developed

---
+++ Assets/Scripts/Children/Punch.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Punch : MonoBehaviour
+{
+    private Player1_Parent Parent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Parent = GameObject.FindObjectOfType<Player1_Parent>();
+    }
+
+    public IEnumerator PunchCo()
+    {
+        //Parent.animator.SetBool("punching",true);
+        Parent.currentState = PlayerState.punch;
+        yield return null;
+        //Parent.animator.SetBool("punching", false);
+        yield return new WaitForSeconds(0.5f);
+    }
+}
---
+++ Assets/Scripts/Parent/Player1_Parent.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PlayerState
+{
+    walk,
+    punch,
+    kick,
+    projectile,
+    uppercut,
+    spotdodge,
+    Hspotdodge,
+    Fdash,
+    Bdash,
+    block,
+    hit
+}
+
+public class Player1_Parent : MonoBehaviour
+{
+    public PlayerState currentState;
+    public Rigidbody2D rigidbody;
+    public Animator animator;
+    private Punch attack1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+
+        attack1 = GameObject.FindObjectOfType<Punch>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /* switch ()
+         {
+             case Punch:
+
+                 break;
+         }*/
+
+        if (Input.GetButtonDown("Punch"))
+            StartCoroutine(attack1.PunchCo());
+    }
+}
