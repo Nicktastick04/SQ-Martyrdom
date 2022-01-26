aed81f8ef02b17b68379a4ed6911714037af811f
Jacob McCormick <jmmcc52@hotmail.com>
2022-01-26 01:10:32 -0800

    Camera Movement for 1 Character
    the camera now follows the character properly, the stage has a floor and the camera has stage limits (easily removable if we don't want it).

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+
+    public Transform target;
+    public float smoothing;
+    public Vector2 maxPosition;
+    public Vector2 minPosition;
+
+    
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (transform.position != target.position)
+        {
+            Vector2 targetPosition = new Vector2(target.position.x,
+                                                 target.position.y);
+            targetPosition.x = Mathf.Clamp(targetPosition.x,
+                                           minPosition.x,
+                                           maxPosition.x);
+            targetPosition.y = Mathf.Clamp(targetPosition.y,
+                                           minPosition.y,
+                                           maxPosition.y);
+            transform.position = Vector2.Lerp(transform.position,
+                targetPosition, smoothing);
+        }
+    }
+}
